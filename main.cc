d34b543211cb8ba1730bc9746e52d56d52d53858
flosacca <flosacca@gmail.com>
2021-11-10 22:14:03 +0800

    Abstract Pixel class in a bad way

--- main.cc
+++ main.cc
@@ -187,114 +187,145 @@ Sphere spheres[] = {//Scene: radius, position, emission, color, material
 };
 // }}}
 
-#define rand() erand48(Xi)
+struct Sampler {
+    uint16_t* buf;
 
-Vec radiance(const Ray& r, int depth, uint16_t* Xi) {
-    double t = INF;
-    Vec n;
-    const Sphere* obj = nullptr;
-    for (const Sphere& e : spheres) {
-        std::optional<std::pair<double, LocalData>> p = e.find(r);
-        if (p && p->first < t) {
-            t = p->first;
-            n = p->second.n;
-            obj = &e;
+    Sampler(uint16_t* buf) : buf(buf) {}
+
+    double uniform() {
+        return erand48(buf);
+    }
+
+    double triangle() {
+        double x = 2 * uniform() - 1;
+        return (1 - glm::sqrt(1 - glm::abs(x))) * glm::sign(x);
+    }
+
+    Vec radiance(const Ray& r, int depth) {
+        double t = INF;
+        Vec n;
+        const Sphere* obj = nullptr;
+        for (const Sphere& e : spheres) {
+            std::optional<std::pair<double, LocalData>> p = e.find(r);
+            if (p && p->first < t) {
+                t = p->first;
+                n = p->second.n;
+                obj = &e;
+            }
+        }
+        if (!obj) {
+            return {};
+        }
+        Vec x = r(t);
+        bool into = glm::dot(n, r.d) < 0;
+        Vec nl = into ? n : -n;
+        Vec f = obj->c;
+        double p = glm::compMax(f);
+        ++depth;
+        if (depth > 5) {
+            if (this->uniform() > p) {
+                return obj->e;
+            }
+            f /= p;
+        }
+        if (obj->m == DIFFUSE) {
+            double r1 = 2 * glm::pi<double>() * this->uniform();
+            double r2 = this->uniform();
+            double r2s = sqrt(r2);
+            Vec w = nl;
+            Vec u = glm::normalize(glm::cross(fabs(w.x) > 0.1 ? Vec(0, 1, 0) : Vec(1, 0, 0), w));
+            Vec v = glm::cross(w, u);
+            Vec d = glm::normalize(u * glm::cos(r1) * r2s + v * glm::sin(r1) * r2s + w * glm::sqrt(1 - r2));
+            return obj->e + f * radiance(Ray(x, d), depth);
+        }
+        Ray reflRay(x, glm::reflect(r.d, n));
+        if (obj->m == SPECULAR) {
+            return obj->e + f * radiance(reflRay, depth);
+        }
+        double nc = 1;
+        double nt = 1.5;
+        double eta = nc / nt;
+        double nnt = into ? eta : 1 / eta;
+        double ddn = glm::dot(r.d, nl);
+        double cos2t = 1 - nnt * nnt * (1 - ddn * ddn);
+        if (cos2t < 0) {
+            return obj->e + f * radiance(reflRay, depth);
+        }
+        Vec tdir = glm::normalize(r.d * nnt - nl * (ddn * nnt + glm::sqrt(cos2t)));
+        double a = nt - nc;
+        double b = nt + nc;
+        double R0 = a * a / (b * b);
+        double c = 1 - (into ? -ddn : glm::dot(tdir, n));
+        double Re = R0 + (1 - R0) * glm::pow(c, 5);
+        double Tr = 1 - Re;
+        double P = 0.25 + 0.5 * Re;
+        if (depth <= 2) {
+            return obj->e + f * (radiance(reflRay, depth) * Re + radiance(Ray(x, tdir), depth) * Tr);
+        }
+        return obj->e + f * (this->uniform() < P ? radiance(reflRay, depth) * (Re / P) : radiance(Ray(x, tdir), depth) * (Tr / (1 - P)));
+    }
+};
+
+struct Pixel {
+    int x, y;
+    Sampler s;
+    Vec c;
+
+    Pixel(int x, int y, uint16_t* buf) :
+        x(x), y(y), s(buf), c(0)
+    {}
+
+    void sample(int n, int w, int h, const Vec& cx, const Vec& cy, const Ray& cam) {
+        for (int sy = 0; sy < 2; ++sy) {
+            for (int sx = 0; sx < 2; ++sx) {
+                Vec r(0);
+                for (int i = 0; i < n; ++i) {
+                    double dx = s.triangle();
+                    double dy = s.triangle();
+                    double lx = ((sx + 0.5 + dx) / 2 + x) / w - 0.5;
+                    double ly = ((sy + 0.5 + dy) / 2 + y) / h - 0.5;
+                    Vec d = glm::normalize(cx * lx + cy * ly + cam.d);
+                    r += s.radiance(Ray(cam.o + d * 140.0, d), 0) * (1.0 / n);
+                }
+                c += glm::clamp(r, 0.0, 1.0) / 4.0;
+            }
         }
     }
-    if (!obj) {
-        return {};
+
+    glm::ivec3 rgb() const {
+        return glm::pow(c, Vec(1 / 2.2)) * 255.0 + 0.5;
     }
-    Vec x = r(t);
-    bool into = glm::dot(n, r.d) < 0;
-    Vec nl = into ? n : -n;
-    Vec f = obj->c;
-    double p = glm::compMax(f);
-    ++depth;
-    if (depth > 5) {
-        if (rand() > p) {
-            return obj->e;
-        }
-        f /= p;
-    }
-    if (obj->m == DIFFUSE) {
-        double r1 = 2 * glm::pi<double>() * rand();
-        double r2 = rand();
-        double r2s = sqrt(r2);
-        Vec w = nl;
-        Vec u = glm::normalize(glm::cross(fabs(w.x) > 0.1 ? Vec(0, 1, 0) : Vec(1, 0, 0), w));
-        Vec v = glm::cross(w, u);
-        Vec d = glm::normalize(u * glm::cos(r1) * r2s + v * glm::sin(r1) * r2s + w * glm::sqrt(1 - r2));
-        return obj->e + f * radiance(Ray(x, d), depth, Xi);
-    }
-    Ray reflRay(x, glm::reflect(r.d, n));
-    if (obj->m == SPECULAR) {
-        return obj->e + f * radiance(reflRay, depth, Xi);
-    }
-    double nc = 1;
-    double nt = 1.5;
-    double eta = nc / nt;
-    double nnt = into ? eta : 1 / eta;
-    double ddn = glm::dot(r.d, nl);
-    double cos2t = 1 - nnt * nnt * (1 - ddn * ddn);
-    if (cos2t < 0) {
-        return obj->e + f * radiance(reflRay, depth, Xi);
-    }
-    Vec tdir = glm::normalize(r.d * nnt - nl * (ddn * nnt + glm::sqrt(cos2t)));
-    double a = nt - nc;
-    double b = nt + nc;
-    double R0 = a * a / (b * b);
-    double c = 1 - (into ? -ddn : glm::dot(tdir, n));
-    double Re = R0 + (1 - R0) * glm::pow(c, 5);
-    double Tr = 1 - Re;
-    double P = 0.25 + 0.5 * Re;
-    if (depth <= 2) {
-        return obj->e + f * (radiance(reflRay, depth, Xi) * Re + radiance(Ray(x, tdir), depth, Xi) * Tr);
-    }
-    return obj->e + f * (rand() < P ? radiance(reflRay, depth, Xi) * (Re / P) : radiance(Ray(x, tdir), depth, Xi) * (Tr / (1 - P)));
-}
+};
 
 int main(int argc, char *argv[]) {
-    int w, h, samps = argc >= 2 ? atoi(argv[1]) / 4 : 50;
+    int w, h, spp = argc >= 2 ? atoi(argv[1]) : 200;
     if (argc >= 4) {
         w = atoi(argv[2]);
         h = atoi(argv[3]);
     } else {
         w = 160, h = 120;
+        // w = 16, h = 12;
     }
     Ray cam(Vec(50, 52, 295.6), glm::normalize(Vec(0, -0.042612, -1)));
     Vec cx = Vec(w * 0.5135 / h, 0, 0);
     Vec cy = glm::normalize(glm::cross(cx, cam.d)) * 0.5135;
-    Vec r;
-    Vec* c = new Vec[w * h];
+    Pixel* p = (Pixel*) malloc(w * h * sizeof(Pixel));
     int k = 0;
-#pragma omp parallel for schedule(dynamic, 1) private(r) num_threads(4)
+#pragma omp parallel for schedule(dynamic, 1) num_threads(4)
     for (int y = 0; y < h; y++) {
-        uint16_t Xi[3] = {0, 0, (uint16_t)((unsigned)y * y * y)};
+        uint16_t buf[3] = {0, 0, (uint16_t)((unsigned)y * y * y)};
         for (int x = 0; x < w; x++) {
             int i = (h - y - 1) * w + x;
-            for (int sy = 0; sy < 2; sy++) {
-                for (int sx = 0; sx < 2; sx++) {
-                    r = Vec();
-                    for (int s = 0; s < samps; s++) {
-                        double r1 = 2 * rand(), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
-                        double r2 = 2 * rand(), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
-                        Vec d = cx * (((sx + .5 + dx) / 2 + x) / w - .5) +
-                                cy * (((sy + .5 + dy) / 2 + y) / h - .5) + cam.d;
-                        d = glm::normalize(d);
-                        r += radiance(Ray(cam.o + d * 140.0, d), 0, Xi) * (1. / samps);
-                    }
-                    c[i] += glm::clamp(r, 0.0, 1.0) / 4.0;
-                }
-            }
-            c[i] = glm::pow(c[i], Vec(1 / 2.2));
+            new (p + i) Pixel(x, y, buf);
+            p[i].sample(spp / 4, w, h, cx, cy, cam);
             k = std::max(k, y * w + x);
-            fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * k / (h * w - 1));
+            fprintf(stderr, "\rRendering (%d spp) %5.2f%%", spp, 100. * k / (h * w - 1));
         }
     }
     FILE *f = fopen("image.ppm", "w");
     fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
     for (int i = 0; i < w * h; i++) {
-        Vec s = c[i] * 255.0 + 0.5;
-        fprintf(f, "%d %d %d ", (int)s.x, (int)s.y, (int)s.z);
+        glm::ivec3 c = p[i].rgb();
+        fprintf(f, "%d %d %d ", c.x, c.y, c.z);
     }
 }
