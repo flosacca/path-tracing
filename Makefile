ec216a091eddf77e3207e1896f15e9733be82fcd
flosacca <flosacca@gmail.com>
2021-11-16 17:21:20 +0800

    Refine Makefile

--- Makefile
+++ Makefile
@@ -1,28 +1,37 @@
-CFLAGS := -O3
-CXXFLAGS := -std=c++14 -fopenmp -O3 -mavx2 -Ilib
+CFLAGS = -O3
+CXXFLAGS = -std=c++14 -fopenmp -O3 -mavx2 $(INCLUDES)
 
-OBJS := build/image.o build/scene.o lib/libyaml-cpp.a
+INCLUDES := -Ilib -Ilib/yaml-cpp/include
+OBJS := build/main.o build/scene.o build/image.o build/libyaml-cpp.a
 PCH := build/all.gch
 
 all: mkdir main
 
-main: $(PCH) $(OBJS) FORCE
-	g++ -o $@ $(CXXFLAGS) -include $(PCH:.gch=) src/main.cc $(OBJS)
+main: $(OBJS)
+	g++ -o $@ $(CXXFLAGS) $^
+
+build/main.o: FORCE
+
+build/%.o: src/%.cc $(PCH)
+	g++ -c -o $@ $(CXXFLAGS) -include $(PCH:.gch=) $<
 
 $(PCH): src/all.h
 	g++ -c -o $@ $(CXXFLAGS) $<
 
-build/scene.o: src/scene.cc $(PCH)
-	g++ -c -o $@ $(CXXFLAGS) -include $(PCH:.gch=) $<
-
 build/image.o: lib/stb_image_write.h
 	gcc -c -o $@ $(CFLAGS) -DSTB_IMAGE_WRITE_IMPLEMENTATION -x c $<
+
+build/libyaml-cpp.a:
+	cd build/yaml-cpp \
+		&& cmake ../../lib/yaml-cpp \
+		&& cmake --build . \
+		&& mv libyaml-cpp.a ..
 
 clean:
 	rm -rf build
 
 mkdir:
-	@mkdir -p build
+	@mkdir -p build/yaml-cpp
 
 FORCE:
 
--- src/scene.h
+++ src/scene.h
@@ -23,43 +23,7 @@ struct Scene {
         models.push_back(std::make_shared<T>(std::forward<Args>(args)...));
     }
 
-    static Scene load(const Yaml& models) {
-        using namespace helper;
-        Scene scene;
-        auto v0 = bind<Vec>;
-        auto s0 = bind<double>;
-        for (const Yaml& t : models) {
-            auto v = v0(t);
-            auto q = bind<std::string>(t);
-            auto k = q("type");
-            Vec c = v("color");
-            Vec e = v("emission");
-            Material m = fetch<Material>(t["material"]);
-            if (k == "plane") {
-                Vec n = v("normal");
-                Vec p = v("point");
-                scene.add<Plane>(n, p, c, m, e);
-            } else if (k == "sphere") {
-                double r = s0(t)("radius");
-                Vec p = v("center");
-                scene.add<Sphere>(r, p, c, m, e);
-            } else if (k == "mesh") {
-                std::vector<Vec> a;
-                std::vector<Mesh::Index> b;
-                loadPly(q("model"), a, b);
-                glm::dmat3 f(fetch(t["scale"], 1.0));
-                if (auto r = t["rotate"]) {
-                    f = glm::rotate(glm::dmat4(f), s0(r)(0), v0(r)(1));
-                }
-                Vec w = v("translate");
-                for (Vec& p : a) {
-                    p = f * p + w;
-                }
-                scene.add<Mesh>(a, b, c, m, e);
-            }
-        }
-        return scene;
-    }
+    static Scene load(const Yaml& models);
 
     static void loadPly(const std::string&, std::vector<Vec>&, std::vector<Mesh::Index>&);
 };
